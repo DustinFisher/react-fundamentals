451137f99ff900f05419b32d0ecfdbc912bcd88b
Dustin Fisher <dustin@dustinfisher.com>
2016-06-25 20:34:38 -0400

    84% complete

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,6 +4,7 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -12,8 +13,8 @@ function puke (object) {
 function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-        <h1>Confirm Players</h1>
+    : <MainContainer>
+      <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player One'>
             <UserDetails info={props.playersInfo[0]} />
@@ -34,7 +35,7 @@ function ConfirmBattle (props) {
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
